e222d6c4d3dc4e446d388e37dc58880a3e694440
cheolgyu <rb7480@knou.ac.kr>
2020-12-08 14:19:03 +0900

    sk-p3-640

--- script/capture.sh
+++ script/capture.sh
@@ -2,29 +2,35 @@
 
 # chmod +x ./capture.sh
 
-sh cap.sh 1   &
-sh cap.sh 2  &
-sh cap.sh 3  &
-sh cap.sh 4  &
-sh cap.sh 5  &
-sh cap.sh 6  &
-sh cap.sh 7  &
-sh cap.sh 8  &
-sh cap.sh 9  &
-sh cap.sh 10 &
-sh cap.sh 11 &
-sh cap.sh 12 &
-sh cap.sh 13 &
-sh cap.sh 14 &
-sh cap.sh 15 &
-sh cap.sh 16 &
-sh cap.sh 17 &
-sh cap.sh 18 &
-sh cap.sh 19 &
-sh cap.sh 20 &
-sh cap.sh 21 &
-sh cap.sh 22 &
-sh cap.sh 23 &
-sh cap.sh 24 &
-sh cap.sh 25 &
-sh cap.sh 26 &
+# sh cap.sh 1   &
+# sh cap.sh 2  &
+# sh cap.sh 3  &
+# sh cap.sh 4  &
+# sh cap.sh 5  &
+# sh cap.sh 6  &
+# sh cap.sh 7  &
+# sh cap.sh 8  &
+# sh cap.sh 9  &
+# sh cap.sh 10 &
+# sh cap.sh 11 &
+# sh cap.sh 12 &
+# sh cap.sh 13 &
+# sh cap.sh 14 &
+# sh cap.sh 15 &
+# sh cap.sh 16 &
+# sh cap.sh 17 &
+# sh cap.sh 18 &
+# sh cap.sh 19 &
+# sh cap.sh 20 &
+# sh cap.sh 21 &
+# sh cap.sh 22 &
+# sh cap.sh 23 &
+# sh cap.sh 24 &
+# sh cap.sh 25 &
+# sh cap.sh 26 &
+sh cap.sh 27 &
+sh cap.sh 28 &
+sh cap.sh 29 &
+sh cap.sh 30 &
+sh cap.sh 31 &
+sh cap.sh 322 &
