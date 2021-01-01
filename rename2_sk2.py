bb9328f03e5ef540dc08505849557ec926c40469
cheolgyu <rb7480@knou.ac.kr>
2021-01-01 22:39:40 +0900

    sk-p7

--- rename2_sk2.py
+++ rename2_sk2.py
@@ -1,14 +1,8 @@
 from datetime import datetime
 import os
 
-img_patrh =  "/home/cheolgyu/workspace/gamebot/gamebot-dataset/ds/ds_sk2/project_1"
-img_patrh_p2 =  "/home/cheolgyu/workspace/gamebot/gamebot-dataset/ds/ds_sk2/project_2"
-img_patrh_p3 =  "/home/cheolgyu/workspace/gamebot/gamebot-dataset/ds/ds_sk2/project_3"
-img_patrh_p4 =  "/home/cheolgyu/workspace/gamebot/gamebot-dataset/ds/ds_sk2/project_4"
-img_patrh_p5 =  "/home/cheolgyu/workspace/gamebot/gamebot-dataset/ds/ds_sk2/project_5"
-img_patrh_p7 =  "/home/cheolgyu/workspace/gamebot/gamebot-dataset/ds/ds_sk2/project_7"
+img_patrh =  "/home/cheolgyu/workspace/gamebot/gamebot-dataset/ds/ds_sk2/project_7"
 
-img_patrh_p3 = img_patrh_p4
 def mk_file(inp_dir,inp_data):
     file_list = os.listdir(inp_dir)
     for file in file_list:
@@ -21,244 +15,193 @@ def mk_file(inp_dir,inp_data):
             data = inp_data
             f.write(data)
             f.close()    
-class p1:
-                    
-    def screen_1():
-        # 창닫기
-        inp_dir = img_patrh+"/1/"
-        inp_data = "1 0.933415 0.906522 0.066176 0.140580\n"
-        mk_file(inp_dir,inp_data)
-        
-    def screen_2():
-        # 창닫기
-        inp_dir = img_patrh+"/2/"
-        inp_data = "2 0.097631 0.057246 0.090686 0.097101\n"
-        mk_file(inp_dir,inp_data)
-        
-    def screen_3():
-        # 창닫기
-        inp_dir = img_patrh+"/3/"
-        inp_data = "3 0.769608 0.775362 0.225490 0.136232\n0.956291 0.050000 0.058007 0.088406\n"
-        mk_file(inp_dir,inp_data)
 
-    def screen_4():
-        # 창닫기
-        inp_dir = img_patrh+"/4/"
-        inp_data = "0.956291 0.050000 0.058007 0.088406\n4 0.645016 0.675362 0.100490 0.081159\n5 0.743056 0.674638 0.097222 0.073913\n"
-        mk_file(inp_dir,inp_data)
+def dir_5():
+    # 창닫기
+    inp_dir = img_patrh+"/5/"
+    inp_data = "0.956291 0.050000 0.058007 0.088406\n6 0.903595 0.771014 0.181373 0.168116\n7 0.230801 0.547826 0.100490 0.168116\n"
+    mk_file(inp_dir,inp_data)
+def dir_1():
+    inp_dir = img_patrh+"/1/"
+    inp_data = "1 0.933415 0.906522 0.066176 0.140580\n26 0.488971 0.445783 0.235860 0.361446\n27 0.828337 0.906627 0.244910 0.122490\n"
+    mk_file(inp_dir,inp_data)
+    
+def dir_2():
+    # 기본모드-가방풀+
+    inp_dir = img_patrh+"/2/"
+    inp_data = "2 0.097631 0.057246 0.090686 0.097101\n25 0.797511 0.901104 0.079186 0.135542\n"
+    mk_file(inp_dir,inp_data)
+def dir_2_1():
+    # 기본모드-가방풀
+    # 추가: 스마트키on
+    inp_dir = img_patrh+"/2_1/"
+    inp_data = "2 0.097631 0.057246 0.090686 0.097101\n16 0.879229 0.790833 0.121241 0.208333\n24 0.036482 0.052711 0.054864 0.095382\n"
+    mk_file(inp_dir,inp_data)
+def dir_2_2():
+    # 기본모드-가방풀
+    # 추가: 스마트키-off
+    inp_dir = img_patrh+"/2_2/"
+    inp_data = "2 0.097631 0.057246 0.090686 0.097101\n15 0.879229 0.790833 0.121241 0.208333\n24 0.036482 0.052711 0.054864 0.095382\n"
+    mk_file(inp_dir,inp_data)
 
-    def screen_5():
-        # 창닫기
-        inp_dir = img_patrh+"/5/"
-        inp_data = "0.956291 0.050000 0.058007 0.088406\n6 0.903595 0.771014 0.181373 0.168116\n7 0.230801 0.547826 0.100490 0.168116\n"
-        mk_file(inp_dir,inp_data)
+def dir_3():
+    # 가방기본화면-분해좌,홈
+    inp_dir = img_patrh+"/3/"
+    inp_data = "3 0.769608 0.775362 0.225490 0.136232\n0.956291 0.050000 0.058007 0.088406\n"
+    mk_file(inp_dir,inp_data)
 
-    def screen_5_1():
-        # 창닫기
-        inp_dir = img_patrh+"/5_1/"
-        inp_data = "0.956291 0.050000 0.058007 0.088406\n6 0.903595 0.771014 0.181373 0.168116\n7 0.184641 0.547101 0.086601 0.157971\n"
-        mk_file(inp_dir,inp_data)    
+def dir_4():
+    # 분해선택활성화-일반,고급,홈
+    inp_dir = img_patrh+"/4/"
+    inp_data = "0.956291 0.050000 0.058007 0.088406\n4 0.645016 0.675362 0.100490 0.081159\n5 0.743056 0.674638 0.097222 0.073913\n"
+    mk_file(inp_dir,inp_data)
 
-    def screen_6():
-        # 창닫기
-        inp_dir = img_patrh+"/6/"
-        inp_data = "8 0.524101 0.500000 0.776961 0.515942\n9 0.613971 0.663043 0.247549 0.155072\n"
-        mk_file(inp_dir,inp_data)
+def dir_5_0_0():
+    # 분해아이템선택화면-일반.on/off,고급.on/off,홈
+    # 추가: 일반.on,고급.off
+    inp_dir = img_patrh+"/5_0_0/"
+    inp_data = "4 0.645016 0.675362 0.100490 0.081159\n0.956291 0.050000 0.058007 0.088406\n6 0.903595 0.771014 0.181373 0.168116\n7 0.230801 0.547826 0.100490 0.168116\n"
+    mk_file(inp_dir,inp_data)
+def dir_5_0_1():
+    # 분해아이템선택화면-일반.on/off,고급.on/off,홈
+    # 추가: 일반.off,고급.on
+    inp_dir = img_patrh+"/5_0_1/"
+    inp_data = "5 0.743056 0.674638 0.097222 0.073913\n0.956291 0.050000 0.058007 0.088406\n6 0.903595 0.771014 0.181373 0.168116\n7 0.230801 0.547826 0.100490 0.168116\n"
+    mk_file(inp_dir,inp_data)    
 
-    def screen_7():
-        # 창닫기
-        inp_dir = img_patrh+"/7/"
-        inp_data = "10 0.428922 0.479710 0.104575 0.171014\n11 0.498775 0.221014 0.178922 0.120290\n"
-        mk_file(inp_dir,inp_data)
-    def screen_7_1():
-        # 창닫기
-        inp_dir = img_patrh+"/7_1/"
-        inp_data = "10 0.361928 0.484783 0.104575 0.169565\n11 0.498775 0.221014 0.178922 0.120290\n"
-        mk_file(inp_dir,inp_data)
+def dir_5_1():
+    # 분해아이템선택화면-,홈 일반.off,고급.off
+    inp_dir = img_patrh+"/5_1/"
+    inp_data = "0.956291 0.050000 0.058007 0.088406\n6 0.903595 0.771014 0.181373 0.168116\n7 0.184641 0.547101 0.086601 0.157971\n"
+    mk_file(inp_dir,inp_data)    
 
-class p2:
-    def p2_skip():
-        #  12-0.스킵
-        inp_dir = img_patrh_p2+"/8/"
-        inp_data = "0 0.928102 0.072500 0.119361 0.085000\n"
-        mk_file(inp_dir,inp_data)
+def dir_6():
+    # 분해팝업.확인
+    # 추가: 홈
+    inp_dir = img_patrh+"/6/"
+    inp_data = "8 0.524101 0.500000 0.776961 0.515942\n9 0.613971 0.663043 0.247549 0.155072\n0 0.956291 0.050000 0.058007 0.088406\n"
+    mk_file(inp_dir,inp_data)
 
-    def p2_skip_popup():
-        # 13-1 확인 14-2 취소 12 스킵
-        inp_dir = img_patrh_p2+"/9/"
-        inp_data = "1 0.613715 0.699846 0.204861 0.121914\n2 0.387587 0.699074 0.203993 0.111111\n0 0.928102 0.072500 0.119361 0.085000\n "
-        mk_file(inp_dir,inp_data)
+def dir_7():
+    # 창닫기
+    inp_dir = img_patrh+"/7/"
+    inp_data = "10 0.428922 0.479710 0.104575 0.171014\n11 0.498775 0.221014 0.178922 0.120290\n"
+    mk_file(inp_dir,inp_data)
+def dir_7_1():
+    # 창닫기
+    inp_dir = img_patrh+"/7_1/"
+    inp_data = "10 0.361928 0.484783 0.104575 0.169565\n11 0.498775 0.221014 0.178922 0.120290\n"
+    mk_file(inp_dir,inp_data)
 
-    def p2_smartkey_off():
-        #  15-3.스마.off
-        inp_dir = img_patrh_p2+"/10/"
-        inp_data = "3 0.879229 0.790833 0.121241 0.208333\n"
-        mk_file(inp_dir,inp_data)
-    def p2_smartkey_on():
-        #  16-4.스마.off
-        inp_dir = img_patrh_p2+"/11/"
-        inp_data = "4 0.879229 0.790833 0.121241 0.208333\n"
-        mk_file(inp_dir,inp_data)
+def dir_8():
+    #  12-0.스킵
+    inp_dir = img_patrh+"/8/"
+    inp_data = "12 0.928102 0.072500 0.119361 0.085000\n"
+    mk_file(inp_dir,inp_data)
 
-##################################################################
-#                   p3 
-##################################################################
-class p3:
-    def p3_dir_1():
-        inp_dir = img_patrh_p3+"/1/"
-        inp_data = "1 0.933415 0.906522 0.066176 0.140580\n"
-        mk_file(inp_dir,inp_data)
-        
-    def p3_dir_2():
-        # 기본모드-가방풀+
-        inp_dir = img_patrh_p3+"/2/"
-        inp_data = "2 0.097631 0.057246 0.090686 0.097101\n"
-        mk_file(inp_dir,inp_data)
-    def p3_dir_2_1():
-        # 기본모드-가방풀
-        # 추가: 스마트키on
-        inp_dir = img_patrh_p3+"/2_1/"
-        inp_data = "2 0.097631 0.057246 0.090686 0.097101\n16 0.879229 0.790833 0.121241 0.208333\n"
-        mk_file(inp_dir,inp_data)
-    def p3_dir_2_2():
-        # 기본모드-가방풀
-        # 추가: 스마트키-off
-        inp_dir = img_patrh_p3+"/2_2/"
-        inp_data = "2 0.097631 0.057246 0.090686 0.097101\n15 0.879229 0.790833 0.121241 0.208333\n"
-        mk_file(inp_dir,inp_data)
+def dir_9():
+    # 13-1 확인 14-2 취소 12 스킵
+    inp_dir = img_patrh+"/9/"
+    inp_data = "13 0.613715 0.699846 0.204861 0.121914\n14 0.387587 0.699074 0.203993 0.111111\n12 0.928102 0.072500 0.119361 0.085000\n "
+    mk_file(inp_dir,inp_data)
 
-    def p3_dir_3():
-        # 가방기본화면-분해좌,홈
-        inp_dir = img_patrh_p3+"/3/"
-        inp_data = "3 0.769608 0.775362 0.225490 0.136232\n0.956291 0.050000 0.058007 0.088406\n"
-        mk_file(inp_dir,inp_data)
+def dir_10():
+    #  15-3.스마.off
+    inp_dir = img_patrh+"/10/"
+    inp_data = "15 0.879229 0.790833 0.121241 0.208333\n24 0.036482 0.052711 0.054864 0.095382\n"
+    mk_file(inp_dir,inp_data)
+def dir_11():
+    #  16-4.스마.on
+    inp_dir = img_patrh+"/11/"
+    inp_data = "16 0.879229 0.790833 0.121241 0.208333\n24 0.036482 0.052711 0.054864 0.095382\n"
+    mk_file(inp_dir,inp_data)
 
-    def p3_dir_4():
-        # 분해선택활성화-일반,고급,홈
-        inp_dir = img_patrh_p3+"/4/"
-        inp_data = "0.956291 0.050000 0.058007 0.088406\n4 0.645016 0.675362 0.100490 0.081159\n5 0.743056 0.674638 0.097222 0.073913\n"
-        mk_file(inp_dir,inp_data)
+def dir_12():
+    #  12 팀편성
+    inp_dir = img_patrh+"/12/"
+    inp_data = "17 0.500000 0.498457 0.913194 0.651235\n18 0.486979 0.650463 0.477431 0.331790\n19 0.601562 0.715278 0.197917 0.158951\n"
+    mk_file(inp_dir,inp_data)
+def dir_13():
+    #  13 미니팝업
+    inp_dir = img_patrh+"/13/"
+    inp_data = "20 0.293837 0.401235 0.556424 0.709877\n21 0.296441 0.109568 0.459201 0.120370\n22 0.492622 0.105710 0.070312 0.128086\n"
+    mk_file(inp_dir,inp_data)
 
-    def p3_dir_5_0_0():
-        # 분해아이템선택화면-일반.on/off,고급.on/off,홈
-        # 추가: 일반.on,고급.off
-        inp_dir = img_patrh_p3+"/5_0_0/"
-        inp_data = "4 0.645016 0.675362 0.100490 0.081159\n0.956291 0.050000 0.058007 0.088406\n6 0.903595 0.771014 0.181373 0.168116\n7 0.230801 0.547826 0.100490 0.168116\n"
-        mk_file(inp_dir,inp_data)
-    def p3_dir_5_0_1():
-        # 분해아이템선택화면-일반.on/off,고급.on/off,홈
-        # 추가: 일반.off,고급.on
-        inp_dir = img_patrh_p3+"/5_0_1/"
-        inp_data = "5 0.743056 0.674638 0.097222 0.073913\n0.956291 0.050000 0.058007 0.088406\n6 0.903595 0.771014 0.181373 0.168116\n7 0.230801 0.547826 0.100490 0.168116\n"
-        mk_file(inp_dir,inp_data)    
+def dir_1_0():
+    inp_dir = img_patrh+"/1_0/"
+    inp_data = "1 0.933415 0.906522 0.066176 0.140580\n26 0.488971 0.445783 0.235860 0.361446\n27 0.828337 0.906627 0.244910 0.122490\n"
+    mk_file(inp_dir,inp_data)
+def dir_1_1_0():
+    inp_dir = img_patrh+"/1_1_0/"
+    inp_data = "0 0.933277 0.924174 0.077703 0.109610\n26 0.488971 0.445783 0.235860 0.361446\n28 0.828337 0.906627 0.244910 0.122490\n"
+    mk_file(inp_dir,inp_data)
+def dir_1_1_1():
+    inp_dir = img_patrh+"/1_1_1/"
+    inp_data = "26 0.488971 0.445783 0.235860 0.361446\n28 0.828337 0.906627 0.244910 0.122490\n"
+    mk_file(inp_dir,inp_data)
+def dir_14():
+    inp_dir = img_patrh+"/14/"
+    inp_data = "23 0.522059 0.492972 0.727376 0.638554\n24 0.036482 0.052711 0.054864 0.095382\n"
+    mk_file(inp_dir,inp_data)
+def dir_15():
+    inp_dir = img_patrh+"/15/"
+    inp_data = "0.956291 0.050000 0.058007 0.088406\n"
+    mk_file(inp_dir,inp_data)
 
-    def p3_dir_5_1():
-        # 분해아이템선택화면-,홈 일반.off,고급.off
-        inp_dir = img_patrh_p3+"/5_1/"
-        inp_data = "0.956291 0.050000 0.058007 0.088406\n6 0.903595 0.771014 0.181373 0.168116\n7 0.184641 0.547101 0.086601 0.157971\n"
-        mk_file(inp_dir,inp_data)    
+def dir_16():
+    inp_dir = img_patrh+"/16/"
+    inp_data = "\n"
+    mk_file(inp_dir,inp_data)
 
-    def p3_dir_6():
-        # 분해팝업.확인
-        # 추가: 홈
-        inp_dir = img_patrh_p3+"/6/"
-        inp_data = "8 0.524101 0.500000 0.776961 0.515942\n9 0.613971 0.663043 0.247549 0.155072\n0 0.956291 0.050000 0.058007 0.088406\n"
-        mk_file(inp_dir,inp_data)
-
-    def p3_dir_7():
-        # 창닫기
-        inp_dir = img_patrh_p3+"/7/"
-        inp_data = "10 0.428922 0.479710 0.104575 0.171014\n11 0.498775 0.221014 0.178922 0.120290\n"
-        mk_file(inp_dir,inp_data)
-    def p3_dir_7_1():
-        # 창닫기
-        inp_dir = img_patrh_p3+"/7_1/"
-        inp_data = "10 0.361928 0.484783 0.104575 0.169565\n11 0.498775 0.221014 0.178922 0.120290\n"
-        mk_file(inp_dir,inp_data)
-
-    def p3_skip():
-        #  12-0.스킵
-        inp_dir = img_patrh_p3+"/8/"
-        inp_data = "12 0.928102 0.072500 0.119361 0.085000\n"
-        mk_file(inp_dir,inp_data)
-
-    def p3_skip_popup():
-        # 13-1 확인 14-2 취소 12 스킵
-        inp_dir = img_patrh_p3+"/9/"
-        inp_data = "13 0.613715 0.699846 0.204861 0.121914\n14 0.387587 0.699074 0.203993 0.111111\n12 0.928102 0.072500 0.119361 0.085000\n "
-        mk_file(inp_dir,inp_data)
-
-    def p3_smartkey_off():
-        #  15-3.스마.off
-        inp_dir = img_patrh_p3+"/10/"
-        inp_data = "15 0.879229 0.790833 0.121241 0.208333\n"
-        mk_file(inp_dir,inp_data)
-    def p3_smartkey_on():
-        #  16-4.스마.on
-        inp_dir = img_patrh_p3+"/11/"
-        inp_data = "16 0.879229 0.790833 0.121241 0.208333\n"
-        mk_file(inp_dir,inp_data)
-
-class p4:
-    def reg_team():
-        #  12 팀편성
-        inp_dir = img_patrh_p5+"/12/"
-        inp_data = "17 0.500000 0.498457 0.913194 0.651235\n18 0.486979 0.650463 0.477431 0.331790\n19 0.601562 0.715278 0.197917 0.158951\n"
-        mk_file(inp_dir,inp_data)
-    def mini_popup_close():
-        #  13 미니팝업
-        inp_dir = img_patrh_p4+"/13/"
-        inp_data = "20 0.293837 0.401235 0.556424 0.709877\n21 0.296441 0.109568 0.459201 0.120370\n22 0.492622 0.105710 0.070312 0.128086\n"
-        mk_file(inp_dir,inp_data)
-
-class p5:
-    def dir_1_0():
-        inp_dir = img_patrh_p5+"/1_0/"
-        inp_data = "\n"
-        mk_file(inp_dir,inp_data)
-    def dir_1_1_0():
-        inp_dir = img_patrh_p5+"/1_1_0/"
-        inp_data = "0 0.933277 0.924174 0.077703 0.109610\n"
-        mk_file(inp_dir,inp_data)
-    def dir_1_1_1():
-        inp_dir = img_patrh_p5+"/1_1_1/"
-        inp_data = "\n"
-        mk_file(inp_dir,inp_data)
-    def dir_14():
-        inp_dir = img_patrh_p5+"/14/"
-        inp_data = "23 0.522059 0.492972 0.727376 0.638554\n"
-        mk_file(inp_dir,inp_data)
-    def dir_15():
-        inp_dir = img_patrh_p5+"/15/"
-        inp_data = "0.956291 0.050000 0.058007 0.088406\n"
-        mk_file(inp_dir,inp_data)
-
-    def dir_16():
-        inp_dir = img_patrh_p5+"/16/"
-        inp_data = "\n"
-        mk_file(inp_dir,inp_data)
-
-class p7:
-    def dir_17():
-        inp_dir = img_patrh_p5+"/17/"
-        inp_data = "24 0.036482 0.052711 0.054864 0.095382\n"
-        mk_file(inp_dir,inp_data)
-    def dir_18():
-        inp_dir = img_patrh_p5+"/18/"
-        inp_data = "25 0.797511 0.901104 0.079186 0.135542\n"
-        mk_file(inp_dir,inp_data)
-    def dir_19():
-        inp_dir = img_patrh_p5+"/19/"
-        inp_data = "26 0.488971 0.445783 0.235860 0.361446\n27 0.828337 0.906627 0.244910 0.122490\n"
-        mk_file(inp_dir,inp_data)
-    def dir_20():
-        inp_dir = img_patrh_p5+"/20/"
-        inp_data = "24 0.036482 0.052711 0.054864 0.095382\n15 0.879229 0.790833 0.121241 0.208333\n"
-        mk_file(inp_dir,inp_data)
-    def dir_21():
-        inp_dir = img_patrh_p5+"/21/"
-        inp_data = "26 0.488971 0.445783 0.235860 0.361446\n28 0.828337 0.906627 0.244910 0.122490\n"
-        mk_file(inp_dir,inp_data)
+def dir_17():
+    inp_dir = img_patrh+"/17/"
+    inp_data = "24 0.036482 0.052711 0.054864 0.095382\n"
+    mk_file(inp_dir,inp_data)
+def dir_18():
+    inp_dir = img_patrh+"/18/"
+    inp_data = "25 0.797511 0.901104 0.079186 0.135542\n"
+    mk_file(inp_dir,inp_data)
+def dir_19():
+    inp_dir = img_patrh+"/19/"
+    inp_data = "26 0.488971 0.445783 0.235860 0.361446\n27 0.828337 0.906627 0.244910 0.122490\n"
+    mk_file(inp_dir,inp_data)
+def dir_20():
+    inp_dir = img_patrh+"/20/"
+    inp_data = "24 0.036482 0.052711 0.054864 0.095382\n15 0.879229 0.790833 0.121241 0.208333\n"
+    mk_file(inp_dir,inp_data)
+def dir_21():
+    inp_dir = img_patrh+"/21/"
+    inp_data = "26 0.488971 0.445783 0.235860 0.361446\n28 0.828337 0.906627 0.244910 0.122490\n"
+    mk_file(inp_dir,inp_data)
 
 
-
+dir_17()   
+dir_18()   
+dir_19()   
+dir_20()   
+dir_21()   
+dir_16()   
+dir_15()   
+dir_14()   
+dir_13()   
+dir_12()   
+dir_11()   
+dir_10()   
+dir_9()    
+dir_8()    
+dir_7()    
+dir_7_1()  
+dir_6()    
+dir_5_1()  
+dir_5_0_1()
+dir_5_0_0()
+dir_4()    
+dir_3()    
+dir_2_2()  
+dir_2_1()  
+dir_2()    
+dir_1()    
+dir_1_0()
+dir_1_1_0()
+dir_1_1_1()
+  
--- yolo/label_after2.py
+++ yolo/label_after2.py
@@ -9,16 +9,18 @@ import math
 # 스크린별로 분류해 폴더로 저장후에 
 # project_1/1~7 수동으로 바꾸면서 valid.txt 업데이트 시키기 
 # open a모드(파일 내용 추가)로 되어있는지 보기  
-project="gotgl"
-project_id="p1"
+project="sk2"
+project_id="p7"
 ratio=0.1
-img_path = "/home/cheolgyu/workspace/gamebot/gamebot-dataset/ds/ds_gotgl/p1/"
-txt_path = "/home/cheolgyu/workspace/gamebot/gamebot-dataset/ds/ds_gotgl/p1/"
-project_path = "/home/cheolgyu/workspace/gamebot/gamebot-yolo/workspace/gotgl/p1"
+img_path = "/home/cheolgyu/workspace/gamebot/gamebot-dataset/ds/ds_sk2/project_7/"
+txt_path = "/home/cheolgyu/workspace/gamebot/gamebot-dataset/ds/ds_sk2/project_7/"
+project_path = "/home/cheolgyu/workspace/gamebot/gamebot-yolo/workspace/sk2/project_7"
 project_train_file= project_path+"/train.txt"
 project_valid_file= project_path+"/valid.txt"
-dir_list=["gotgl_001","gotgl_002","gotgl_003","gotgl_004","gotgl_007","gotgl_009"]
+dir_list=["1","10","11","12","13","14","15","16","17","18","19","1_0","1_1_0","1_1_1","2","20","21","2_1","2_2","3","4","5","5_0_0","5_0_1","5_1","6","7","7_1","8","9"]
+
 def one_dir(inp_img_path,inp_txt_path):
+    print(inp_txt_path)
     txt_file_list = os.listdir(inp_txt_path)
 
     all_list=[]
